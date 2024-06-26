7c51b4d5c645165f60e626b9e6d2536d1d2d2d0e
Chun-Wei-Tseng <63666127+Chun-Wei-Tseng@users.noreply.github.com>
2024-06-26 00:08:01 -0700

    Add files via upload

---
+++ CountToTwentyFour/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+for (int increment = 1; increment <= 4; increment++)
+{
+    for (int i = 0; i <= 24; i += increment)
+    {
+        Console.Write(i);
+        if (i + increment <= 24)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine();
+}
---
+++ GuessNumberBetweenOneAndThree/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int correctNumber = new Random().Next(3) + 1;
+Console.WriteLine("Guess a number between 1 and 3:");
+int userInput = int.Parse(Console.ReadLine());
+if (userInput < 1 || userInput > 3)
+{
+    Console.WriteLine("Your guess is outside the range of valid numbers.");
+}
+else if (userInput < correctNumber)
+{
+    Console.WriteLine("Your guess is too low.");
+}
+else if (userInput > correctNumber)
+{
+    Console.WriteLine("Your guess is too high.");
+}
+else
+{
+    Console.WriteLine("Congratulations! You guessed the correct number.");
+}

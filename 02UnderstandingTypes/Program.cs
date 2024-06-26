01b0b38a227f9edd638893998ea99fd6a924db8b
Chun-Wei-Tseng <63666127+Chun-Wei-Tseng@users.noreply.github.com>
2024-06-26 00:03:26 -0700

    Add files via upload

---
+++ 02UnderstandingTypes/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Data Type Information:");
+Console.WriteLine("======================\n");
+// sbyte
+Console.WriteLine("sbyte:");
+Console.WriteLine($"Size (bytes): {sizeof(sbyte)}");
+Console.WriteLine($"Minimum value: {sbyte.MinValue}");
+Console.WriteLine($"Maximum value: {sbyte.MaxValue}\n");
+// byte
+Console.WriteLine("byte:");
+Console.WriteLine($"Size (bytes): {sizeof(byte)}");
+Console.WriteLine($"Minimum value: {byte.MinValue}");
+Console.WriteLine($"Maximum value: {byte.MaxValue}\n");
+// short
+Console.WriteLine("short:");
+Console.WriteLine($"Size (bytes): {sizeof(short)}");
+Console.WriteLine($"Minimum value: {short.MinValue}");
+Console.WriteLine($"Maximum value: {short.MaxValue}\n");
+// ushort
+Console.WriteLine("ushort:");
+Console.WriteLine($"Size (bytes): {sizeof(ushort)}");
+Console.WriteLine($"Minimum value: {ushort.MinValue}");
+Console.WriteLine($"Maximum value: {ushort.MaxValue}\n");
+// int
+Console.WriteLine("int:");
+Console.WriteLine($"Size (bytes): {sizeof(int)}");
+Console.WriteLine($"Minimum value: {int.MinValue}");
+Console.WriteLine($"Maximum value: {int.MaxValue}\n");
+// uint
+Console.WriteLine("uint:");
+Console.WriteLine($"Size (bytes): {sizeof(uint)}");
+Console.WriteLine($"Minimum value: {uint.MinValue}");
+Console.WriteLine($"Maximum value: {uint.MaxValue}\n");
+// long
+Console.WriteLine("long:");
+Console.WriteLine($"Size (bytes): {sizeof(long)}");
+Console.WriteLine($"Minimum value: {long.MinValue}");
+Console.WriteLine($"Maximum value: {long.MaxValue}\n");
+// ulong
+Console.WriteLine("ulong:");
+Console.WriteLine($"Size (bytes): {sizeof(ulong)}");
+Console.WriteLine($"Minimum value: {ulong.MinValue}");
+Console.WriteLine($"Maximum value: {ulong.MaxValue}\n");
+// float
+Console.WriteLine("float:");
+Console.WriteLine($"Size (bytes): {sizeof(float)}");
+Console.WriteLine($"Minimum value: {float.MinValue}");
+Console.WriteLine($"Maximum value: {float.MaxValue}\n");
+// double
+Console.WriteLine("double:");
+Console.WriteLine($"Size (bytes): {sizeof(double)}");
+Console.WriteLine($"Minimum value: {double.MinValue}");
+Console.WriteLine($"Maximum value: {double.MaxValue}\n");
+// decimal
+Console.WriteLine("decimal:");
+Console.WriteLine($"Size (bytes): {sizeof(decimal)}");
+Console.WriteLine($"Minimum value: {decimal.MinValue}");
+Console.WriteLine($"Maximum value: {decimal.MaxValue}\n");
